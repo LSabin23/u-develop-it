2d2f2f11a6157f81053fcfca2572396cbeb66c7f
unknown <44147809+LSabin23@users.noreply.github.com>
2021-12-17 20:06:40 -0500

    added dotenv package, added connection to election db and query for candidates table

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+const mysql = require('mysql2')
 const express = require('express')
 
 const PORT = process.env.PORT || 3001
@@ -6,6 +8,22 @@ const app = express()
 // Express middleware
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    // dotenv protected values
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+)
+
+db.query('SELECT * FROM candidates', (err, rows) => {
+  console.log(rows)
+})
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
