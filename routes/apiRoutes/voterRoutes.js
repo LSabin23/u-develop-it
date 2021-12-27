350f2a40567c6eed9742d4005868a4de66b840a9
unknown <44147809+LSabin23@users.noreply.github.com>
2021-12-26 21:54:01 -0500

    added vote routes to return vote totals

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const res = require('express/lib/response')
 const router = express.Router()
 const db = require('../../db/connection')
 const inputCheck = require('../../utils/inputCheck')
